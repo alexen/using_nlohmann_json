681c807c60030f3dfb35bd4c6f3f18c09c42b24c
Enakaev Aleksey <enakaev.aleksey@yandex.ru>
2024-10-01 17:45:46 +0300

    serialize: Наброски для {де}сериализации Payload

--- src/serialize.h
+++ src/serialize.h
@@ -22,3 +22,29 @@ void to_json( nlohmann::json& json, const Header& header );
 std::ostream& operator<<( std::ostream&, const Bytes& );
 std::ostream& operator<<( std::ostream&, const Header& );
 
+
+using StringVector = std::vector< std::string >;
+
+struct Payload {
+     StringVector aud;
+     std::string client_id;
+     std::string client_name;
+     std::string client_ogrnip;
+     std::string cti;
+     std::string exp;
+     std::string iat;
+     std::string iss;
+//     obtained_consent_list;
+     std::string req_cti;
+//     requested_consent_list;
+//     resource;
+//     urn_esia_trust;
+};
+
+
+void from_json( const nlohmann::json& json, Payload& header );
+void to_json( nlohmann::json& json, const Payload& header );
+
+
+std::ostream& operator<<( std::ostream&, const StringVector& );
+std::ostream& operator<<( std::ostream&, const Payload& );
